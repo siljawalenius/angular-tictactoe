48831fcfb33bac7c8b9db7f4972d325dd6f8a0be
Silja Walenius <siljawalenius@gmail.com>
2020-09-15 17:40:54 -0400

    start of project

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+//decorators are things that start with an @
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent {
+
+  @Input() value : 'X' | 'O';
+}
